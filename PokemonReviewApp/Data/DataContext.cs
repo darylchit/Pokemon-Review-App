397a225eb1af317c9088bb993b2b7e90b61e5eda
darylchit <darylchit@gmail.com>
2026-02-13 21:48:02 -0800

    Initial commit - models, migrations

---
+++ PokemonReviewApp/Data/DataContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+
+// Create after creating models
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContext : DbContext // 1 DB SETUP
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) // 2
+        {
+            //DataContext inherits from DbContext, this is a constructor that tells the database your configurations/settings for your app
+        }
+
+        public DbSet<Category> Categories { get; set; } //DbSet tells DB what tables you have and what objects they are, allowing querying in C# // 3
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //many to many, how you go in and customize tables without going into DB
+        {
+            //Pokemon Category Many to Many
+            modelBuilder.Entity<PokemonCategory>() // 4
+                .HasKey(pc => new { pc.PokemonId, pc.CategoryId }); // Link two IDs together or EF won't know
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(c => c.CategoryId);
+
+            //Pokemon Owner Many to Many
+            modelBuilder.Entity<PokemonOwner>() // 5 ----> 6 is in Program.cs
+                .HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(c => c.OwnerId);
+        }
+
+    }
+}
---
+++ PokemonReviewApp/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp;
+using PokemonReviewApp.Data;
+
+var builder = WebApplication.CreateBuilder(args); // Create a builder for the web application, a builder is used to configure services and the app
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<Seed>(); // Seed service, Dependency Injection
+// Transient means create a new instance every time it's requested
+// Scoped means create one instance per request
+// Singleton means create one instance for the entire application lifetime
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options => // 6
+{ // Telling our DataContext/DbContext how to connect to our DB
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
+});
+
+var app = builder.Build(); // Build the web application
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata") // Seed data if the argument is passed
+    SeedData(app);
+void SeedData(IHost app) // Injecting service into Program.cs
+{
+    var scopedFactory = app.Services.GetRequiredService<IServiceScopeFactory>(); // Create a scope to get the service
+
+    using (var scope = scopedFactory.CreateScope()) // Create a scope
+    {
+        var service = scope.ServiceProvider.GetRequiredService<Seed>(); // Get the Seed service
+        service.SeedDataContext(); // Call the SeedDataContext method to seed the database
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
